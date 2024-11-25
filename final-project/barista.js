6e493f957d9388ee62d3b1986ee844628630d234
naijangh <naijangha@gmail.com>
2024-11-25 00:44:05 -0500

    major css updates

---
+++ final-project/barista.js
@@ -0,0 +1,9 @@
+/*what needs to happen here:
+- micron.js for little animations
+- random generator function for drinks on today's orders
+- function to populate html with random drinks
+- each image needs a js counterpart to trigger a response when clicked?
+- way to store which element was clicked and calculate if it was the correct one (like hangman guessing game js or tic tac toe?)
+- basic function that takes user input and sees if it is correct and in the right order (1 then 2 then 3) and using if..then statements
+
+*/
